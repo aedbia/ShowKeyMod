b7758e34c1f5515086f5f1f6c5a897041d6f0801
aedbia <906320722@qq.com>
2024-01-17 18:06:15 +0800

    1.18.0-18.0.3

--- src/main/java/aedbia/showKey/client/gui/ShowKeyGui.java
+++ src/main/java/aedbia/showKey/client/gui/ShowKeyGui.java
@@ -2,9 +2,8 @@ package aedbia.showKey.client.gui;
 
 import aedbia.showKey.KeyInfoHelper;
 import aedbia.showKey.ShowKey;
-import aedbia.showKey.ShowKeyConfig;
 import aedbia.showKey.compatible.keybindsGalore.KeybindsGaloreCompatible;
-import aedbia.showKey.mixins.SKMixins;
+import aedbia.showKey.configs.ShowKeyConfig;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.KeyMapping;
@@ -20,8 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -29,23 +26,26 @@ public class ShowKeyGui implements IIngameOverlay {
 
     private static final ResourceLocation button_down = new ResourceLocation(ShowKey.MODID, "textures/gui/button_down.png");
     private static final ResourceLocation button_release = new ResourceLocation(ShowKey.MODID, "textures/gui/button_release.png");
-    private static final ScheduledThreadPoolExecutor scheduled = new ScheduledThreadPoolExecutor(1);
     private final Minecraft mc = Minecraft.getInstance();
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private final String id;
+    public boolean onRender = false;
     private List<KeyMapping> displayKeyMappings = new ArrayList<>();
     private List<KeyMapping> modifierMappings = new ArrayList<>();
     private int activeKeyCount = 10;
 
+    private int showCount = 7;
+
     public ShowKeyGui() {
         this.id = "keys";
-        scheduled.scheduleAtFixedRate(this::tick, 0, 100, TimeUnit.MILLISECONDS);
     }
 
     @SuppressWarnings("unused")
     @Override
     public void render(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int screenWidth, int screenHeight) {
-        //LogUtils.getLogger().debug("1111adada");
+        onRender = true;
         double scale = ShowKeyConfig.UIScaleNumber;
+        int mode = ShowKeyConfig.displayMode;
         if (scale < 0.1) {
             scale = 0.5;
         }
@@ -53,15 +53,14 @@ public class ShowKeyGui implements IIngameOverlay {
         double wight = (double) screenWidth / 8;
         double height = (double) screenHeight / (3 * scale);
         int count = 0;
-        int showCount = (int) (7 / scale);
         double y = (screenHeight / scale - height / showCount);
         boolean right = false;
         poseStack.scale((float) scale, (float) scale, 1f);
         for (KeyMapping keyMapping : modifierMappings) {
-            if (count >= 2 * showCount) {
+            if (count >= 2 * showCount || (mode != 0 && count >= showCount)) {
                 break;
             }
-            if (activeKeyCount / 2 >= 1 && count == activeKeyCount / 2) {
+            if ((mode == 0 && activeKeyCount / 2 >= 1 && count == activeKeyCount / 2) || (mode == 1 && count == 0)) {
                 x = screenWidth / scale - wight;
                 y = (screenHeight / scale - height / showCount);
                 right = true;
@@ -74,10 +73,10 @@ public class ShowKeyGui implements IIngameOverlay {
         }
         if (count == 0) {
             for (KeyMapping keyMapping : displayKeyMappings) {
-                if (count >= 2 * showCount) {
+                if (count >= 2 * showCount || (mode != 0 && count >= showCount)) {
                     break;
                 }
-                if (activeKeyCount / 2 >= 1 && count == activeKeyCount / 2) {
+                if ((mode == 0 && activeKeyCount / 2 >= 1 && count == activeKeyCount / 2) || (mode == 1 && count == 0)) {
                     x = screenWidth / scale - wight;
                     y = (screenHeight / scale - height / showCount);
                     right = true;
@@ -98,7 +97,7 @@ public class ShowKeyGui implements IIngameOverlay {
         if (key.length() <= 1) {
             key = key.toUpperCase();
         }
-        boolean isKeyDown = ((SKMixins.AccessorKeyMapping) keyMapping).getIsDown();
+        boolean isKeyDown = keyMapping.isDown();
         int color = isKeyDown ? 0x808080 : 0xFFFFFF;
         String keyName = new TranslatableComponent(keyMapping.getName()).getString();
         int keyLoc = (int) (right ? (x + wight * 3 / 4) : (x + wight / 4));
@@ -112,13 +111,24 @@ public class ShowKeyGui implements IIngameOverlay {
 
     }
 
-    private void tick() {
-            KeybindsGaloreCompatible.receiveIMCMessage();
-            List<KeyMapping> list = Arrays.stream(mc.options.keyMappings).filter(KeyInfoHelper::isShowKeyMapping).collect(Collectors.toMap(KeyMapping::getKey, Function.identity(), (a, b) -> a)).values().stream().toList();
-            modifierMappings = list.stream()
-                    .filter(a -> a.getKeyModifier() != KeyModifier.NONE).sorted(Comparator.comparingInt(a -> -a.getKey().getValue())).toList();
-
-            displayKeyMappings = list.stream()
-                    .filter(a -> a.getKeyModifier() == KeyModifier.NONE).sorted(Comparator.comparingInt(a -> -a.getKey().getValue())).toList();
+    public void tick() {
+        double scale = ShowKeyConfig.UIScaleNumber;
+        showCount = (int) (7 / scale);
+        KeybindsGaloreCompatible.receiveIMCMessage();
+        List<KeyMapping> list = Arrays.stream(mc.options.keyMappings).filter(KeyInfoHelper::isShowKeyMapping).collect(Collectors.toMap(KeyMapping::getKey, Function.identity(), (a, b) -> a)).values().stream().toList();
+        List<KeyMapping> modifier = list.stream()
+                .filter(a -> a.getKeyModifier() != KeyModifier.NONE).sorted(Comparator.comparingInt(a -> -a.getKey().getValue())).toList();
+        if (modifier.size() > showCount) {
+            modifierMappings = modifier.subList(modifier.size() - showCount, modifier.size());
+        } else {
+            modifierMappings = modifier;
+        }
+        List<KeyMapping> noModifier = list.stream()
+                .filter(a -> a.getKeyModifier() == KeyModifier.NONE).sorted(Comparator.comparingInt(a -> -a.getKey().getValue())).toList();
+        if (noModifier.size() > showCount) {
+            displayKeyMappings = noModifier.subList(noModifier.size() - showCount, noModifier.size());
+        } else {
+            displayKeyMappings = noModifier;
+        }
     }
 }
---
+++ src/main/java/aedbia/showKey/configs/KeyConfig.java
@@ -0,0 +1,154 @@
+package aedbia.showKey.configs;
+
+import aedbia.showKey.ShowKey;
+import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import com.electronwill.nightconfig.toml.TomlFormat;
+import com.electronwill.nightconfig.toml.TomlParser;
+import com.electronwill.nightconfig.toml.TomlWriter;
+import net.minecraft.network.chat.TranslatableComponent;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+@SuppressWarnings({"unused", "FieldCanBeLocal", "FieldMayBeFinal", "ResultOfMethodCallIgnored", "UnusedReturnValue"})
+public class KeyConfig {
+    private static List<KeyConfig> keyConfigs = new ArrayList<>();
+    private final Path path;
+    private final String fileName;
+    private final String fileFullName;
+    private CommentedConfig config;
+    private List<Value<?>> values = new ArrayList<>();
+
+    public KeyConfig(Path path, String fileName) {
+        this.path = path;
+        this.fileName = fileName;
+        config = TomlFormat.newConfig();
+        fileFullName = String.format(Locale.ROOT, "%s.toml", removeChar(new TranslatableComponent(fileName).getString()));
+        keyConfigs.add(this);
+    }
+
+    private static String removeChar(String fileName) {
+        String b = fileName;
+        String a = "/:\"|<>*?\\\\";
+        for (char c : a.toCharArray()) {
+            b = b.replace(Character.toString(c), "");
+        }
+        return b;
+    }
+
+    public static void loadAll() {
+        keyConfigs.forEach(KeyConfig::load);
+    }
+
+    public static Value<?> getValue(String fileName, String path) {
+        return getValue(null, fileName, path);
+    }
+
+    public static Value<?> getValue(Path local, String fileName, String path) {
+        List<KeyConfig> k = keyConfigs.stream().filter(a -> Objects.equals(a.fileName, fileName) && (local == null || a.path == local)).toList();
+        if (k.isEmpty()) {
+            return null;
+        }
+        KeyConfig keyConfig = k.get(0);
+        return keyConfig.getValue(path);
+    }
+
+    public void build() {
+        if (!path.resolve(fileFullName).toFile().exists()) {
+            try {
+                path.resolve(fileFullName).toFile().createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (path.resolve(fileFullName).toFile().exists()) {
+            load();
+            write();
+        }
+    }
+
+    private void write() {
+        TomlWriter writer = new TomlWriter();
+        writer.write(config, path.resolve(fileFullName), WritingMode.REPLACE);
+    }
+
+    private void load() {
+        if (!path.toFile().exists()) {
+            boolean a = path.toFile().mkdirs();
+            ShowKey.LOGGER.debug("create configMkd" + a);
+        }
+        if (!path.resolve(fileFullName).toFile().exists()) {
+            try {
+                boolean a = path.resolve(fileFullName).toFile().createNewFile();
+                ShowKey.LOGGER.debug("create config" + a);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        CommentedConfig config1;
+        try (FileReader fileReader = new FileReader(String.valueOf(path.resolve(fileFullName)))) {
+            TomlParser tomlParser = new TomlParser();
+            config1 = tomlParser.parse(fileReader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (config1 != null) {
+            config1.checked();
+            for (Value<?> value : values) {
+                if (config1.contains(value.path)) {
+                    if (config1.get(value.path).getClass() != value.defaultValue.getClass()) {
+                        config1.set(value.path, value.defaultValue);
+                    }
+                    if (config.contains(value.path)) {
+                        config.set(value.path, config1.get(value.path));
+                    } else {
+                        config.add(value.path, config1.get(value.path));
+                    }
+                } else {
+                    config.add(value.path, value.defaultValue);
+
+                }
+                config.setComment(value.path, value.description);
+            }
+        }
+    }
+
+    public List<Value<?>> getValues() {
+        return this.values;
+    }
+
+    public Value<?> getValue(String path) {
+        List<Value<?>> values1 = values.stream().filter(a -> Objects.equals(a.path, path)).toList();
+        if (values1.isEmpty()) {
+            return null;
+        }
+        return values1.get(0);
+    }
+
+    public <T> Value<T> Add(String path, T defaultValue, String description) {
+        return new Value<>(path, defaultValue, description);
+    }
+
+    public class Value<T> {
+        final String path;
+        private final T defaultValue;
+        private final String description;
+
+        Value(String path, T defaultValue, String description) {
+            this.path = path;
+            this.defaultValue = defaultValue;
+            this.description = description;
+            values.add(this);
+        }
+
+        public T get() {
+            return config.get(this.path);
+        }
+    }
+}
