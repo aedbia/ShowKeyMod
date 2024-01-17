aa14e1a3c329e0ae9b5ca5a373b976a15e767b97
aedbia <906320722@qq.com>
2024-01-17 19:29:15 +0800

    1.20.2-20.2.4

--- src/main/java/aedbia/showKey/ShowKey.java
+++ src/main/java/aedbia/showKey/ShowKey.java
@@ -1,55 +1,52 @@
 package aedbia.showKey;
 
 import aedbia.showKey.client.ShowKeyCommandThread;
-import aedbia.showKey.client.gui.ShowKeyGui;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import aedbia.showKey.configs.ShowKeyConfig;
 import com.mojang.logging.LogUtils;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiOverlaysEvent;
 import net.neoforged.neoforge.client.gui.overlay.VanillaGuiOverlay;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
+import java.nio.file.Path;
+
 @Mod(ShowKey.MODID)
 public class ShowKey {
     public static final String MODID = "show_key";
-    @SuppressWarnings("unused")
-    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Path CONFIG_PATCH = FMLPaths.CONFIGDIR.get().resolve(MODID);
 
     public ShowKey(IEventBus bus) {
         NeoForge.EVENT_BUS.addListener(this::onRegisterClientCommands);
-        //NeoForge.EVENT_BUS.register(this);
         bus.register(this);
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ShowKeyConfig.SPEC);
     }
 
-    public void onRegisterClientCommands(RegisterClientCommandsEvent event) {
-        LiteralArgumentBuilder<CommandSourceStack> commands = Commands.literal(MODID).requires(a -> a.hasPermission(2));
-        event.getDispatcher()
-                .register(commands.then(Commands.literal("monitor")
-                        .then(Commands.literal("start").executes(a -> {
-                            new ShowKeyCommandThread();
-                            ShowKeyCommandThread.stop = false;
-                            return 1;
-                        }))));
-        event.getDispatcher()
-                .register(commands.then(Commands.literal("monitor")
-                        .then(Commands.literal("stop").executes(a -> {
-                            ShowKeyCommandThread.stop = true;
-                            return 1;
-                        }))));
-    }
-
     @SubscribeEvent
     public void onRenderBar(RegisterGuiOverlaysEvent event) {
-        ShowKeyGui gui = new ShowKeyGui();
-        event.registerBelow(VanillaGuiOverlay.HOTBAR.id(), gui.id(), gui);
+        event.registerBelow(VanillaGuiOverlay.HOTBAR.id(), KeyInfoHelper.gui.id(), KeyInfoHelper.gui);
+    }
+
+    public void onRegisterClientCommands(RegisterClientCommandsEvent event) {
+        ShowKeyCommandThread.registerCommands(event);
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            ShowKeyConfig.initKeyConfig();
+            KeyInfoHelper.start();
+        }
     }
 }
